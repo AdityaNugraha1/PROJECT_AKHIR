99dadcb1bb00fee37eaa8a0de7dea47d63fa3fef
fzdeltaa <112379648+fzdeltaa@users.noreply.github.com>
2022-11-17 20:52:02 +0700

    git commit

---
+++ keranjang.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Keranjang - Tokopedia</title>
+    <link rel="stylesheet" href="style/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+</head>
+
+<body>
+    <section class="vh-100">
+    <div class="container text-start">
+  <div class="row">
+    <div class="col-1">
+      
+    </div>
+    <div class="col-7">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris iaculis sed est non egestas. Donec id nisl est. Aenean mi ante, gravida eu mollis a, mattis eget felis. Quisque ipsum erat, molestie ut vestibulum quis, aliquam nec erat. Phasellus blandit vestibulum lobortis. Phasellus efficitur ligula ex, vitae vulputate justo pretium ac. Etiam pharetra eleifend lacus ac scelerisque. Nunc interdum fringilla lacus id rutrum. Suspendisse placerat, libero a molestie ornare, purus eros egestas velit, nec volutpat metus elit sollicitudin ipsum. Phasellus congue, mi non aliquet dapibus, enim nulla consequat turpis, non molestie metus tortor sed turpis. Vestibulum dictum et sapien sed luctus. Sed vitae dolor elit.
+
+Maecenas eu pellentesque eros. Praesent ut magna egestas, vehicula neque et, consectetur urna. Donec et felis quam. Fusce scelerisque lorem turpis, non congue purus sagittis et. Sed fringilla nunc risus, eu egestas orci lacinia eleifend. Etiam ac risus scelerisque, tincidunt arcu ac, rhoncus eros. Phasellus eu tristique augue. Vestibulum ultricies felis a nisl feugiat, sit amet vestibulum mauris hendrerit. Aliquam lacinia ultrices nibh, auctor maximus nulla viverra ac. Nullam finibus id dolor quis dictum. Quisque sodales justo tortor, ut feugiat velit venenatis vitae.
+
+In hac habitasse platea dictumst. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Phasellus placerat est et semper aliquet. Aenean rutrum pellentesque neque, ac vestibulum nisl rhoncus eu. Nulla vel purus consectetur, dictum ipsum ac, lobortis lectus. Ut luctus ligula mauris, quis sollicitudin lorem blandit at. Sed eu mauris nec lorem fermentum bibendum. Phasellus varius ante eu felis sollicitudin pretium. Duis fringilla vestibulum diam tempor sagittis. Morbi fringilla risus a quam suscipit volutpat. Cras elementum id nulla ac posuere. Ut euismod ipsum vitae enim luctus, et mattis orci pellentesque. Phasellus ullamcorper felis ligula, a auctor libero volutpat egestas. Vestibulum ante nunc, molestie eget finibus in, aliquam iaculis purus. Mauris laoreet, sem sed malesuada convallis, odio nulla sollicitudin quam, vel volutpat mi velit et est. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+
+Duis volutpat nisi at elit blandit luctus. Nunc aliquam egestas mauris, nec lobortis magna lobortis et. Nulla fringilla ex vitae nunc fringilla, a rutrum lacus scelerisque. Donec velit est, sollicitudin a odio in, placerat lobortis nisl. Aenean finibus rutrum aliquet. Integer sed egestas justo, vel mattis velit. Nulla ornare vestibulum felis, et sagittis quam blandit sit amet.
+
+In tristique ac ex non tempus. Fusce nec nisi eu orci cursus faucibus ut id ipsum. Integer non tempor augue. Vestibulum consequat mattis volutpat. Proin eu volutpat urna, et finibus sem. Sed nec blandit dolor, ac imperdiet dolor. In diam sapien, rhoncus at lacinia sed, dictum nec risus. Sed id accumsan nulla. Phasellus vitae diam eu libero tincidunt gravida non sit amet urna. Maecenas vitae aliquam ipsum, et facilisis urna. Vestibulum efficitur rutrum tortor, et imperdiet turpis. Etiam tellus elit, iaculis dapibus arcu a, suscipit fringilla orci. Vestibulum quis nulla ac dolor consequat varius. Ut convallis, turpis sed porttitor maximus, mi felis mattis mauris, id euismod felis nulla sit amet elit. Aenean tempus sed turpis placerat dictum. Curabitur in blandit diam, sit amet faucibus arcu.
+
+Praesent urna augue, dictum eget venenatis et, aliquam vitae urna. Nullam odio risus, volutpat et sem nec, efficitur tempus sapien. Pellentesque in mollis est. Mauris bibendum bibendum purus, sodales scelerisque mauris. Donec mauris dui, malesuada rutrum massa vitae, auctor porttitor mi. Integer malesuada tristique arcu pulvinar feugiat. Fusce laoreet aliquet nibh, vehicula finibus ante rhoncus viverra. Phasellus aliquam mollis eleifend. Nam turpis purus, pretium quis faucibus eget, luctus eget enim. In iaculis enim tincidunt blandit egestas. Duis a lectus erat. In hac habitasse platea dictumst. Phasellus eget sapien lacus.
+
+Vivamus pretium, augue vel efficitur elementum, ipsum nunc semper sem, eu maximus erat dolor vel dolor. Vestibulum orci urna, dignissim non nisl vel, consectetur mattis velit. Nam commodo finibus neque. Etiam eu mi vel turpis facilisis dapibus. Sed scelerisque ante justo, nec porttitor quam mollis ac. Suspendisse pharetra id dui ac eleifend. Phasellus sed fermentum felis.
+
+Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Duis quam quam, malesuada ut placerat sed, dictum eu felis. Donec facilisis urna a ipsum porta, ac lobortis lacus ultricies. Mauris commodo, enim et tincidunt faucibus, turpis augue viverra magna, id fermentum enim ante non purus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Fusce et leo elit. Curabitur varius sodales diam non ornare. Mauris placerat turpis dui, eget feugiat lorem tincidunt ac. Nam tristique, mauris non porttitor convallis, velit velit lacinia ante, at pharetra sem erat ut est. Integer nec dolor risus. Quisque porta risus tellus, ac tristique est ullamcorper in. Integer semper ac dolor id convallis. Vivamus eros orci, eleifend sed molestie ac, hendrerit vel purus. Suspendisse potenti. Nunc non libero lobortis, ornare justo vel, feugiat leo.
+
+Vivamus sem lacus, fermentum vitae tristique at, pretium in ligula. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent lobortis pulvinar justo id ullamcorper. Nullam et est vel eros cursus posuere sed ac felis. Integer non congue lacus, in pharetra nisl. Ut id eleifend dolor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis facilisis nunc quis magna mollis, in faucibus leo imperdiet. Sed ut tellus varius purus lobortis faucibus.
+
+Vestibulum sem enim, accumsan sed lobortis quis, mollis faucibus nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Quisque molestie, tellus sit amet sagittis suscipit, nulla ligula tempor justo, at tempor magna ipsum sed eros. Interdum et malesuada fames ac ante ipsum primis in faucibus. Maecenas eu sollicitudin sapien. Fusce eget metus sed tortor cursus elementum eu quis mauris. Vivamus a dolor sit amet erat euismod porttitor. Fusce iaculis diam erat, vel iaculis purus sodales ac. Nulla sed egestas velit. Etiam imperdiet vitae augue a dignissim. Pellentesque at tristique purus. Cras congue at purus ac posuere.
+
+
+    </div>
+    <div class="col-4 fixed-top offset-sm-8">
+      Column tes
+    </div>
+  </div>
+</div>
+    </section>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+</body>
+
+</html>

dbf6a4630e69acf5d5f0fddbf974803cac922601
yeheskielaji <94420820+yeheskielaji@users.noreply.github.com>
2022-11-18 07:02:37 +0700

    picture

--- register.php
+++ register.php
@@ -14,7 +14,7 @@
         <div class="container-fluid h-custom">
             <div class="row d-flex justify-content-center align-items-center h-100">
                 <div class="col-md-9 col-lg-6 col-xl-5">
-                    <img src="asset/logo.png" class="rounded img-fluid" alt="medical device">
+                    <img src="asset/medicaltool-icon.png" class="rounded img-fluid" alt="medical device">
                 </div>
                 <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
                     <div class="mx-auto d-block pb-5">

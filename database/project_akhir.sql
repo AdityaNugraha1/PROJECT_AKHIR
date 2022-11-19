92708150c6fffc4a52d438331947d52041dd692c
AdityaNugraha1 <adit.toyan27@gmail.com>
2022-11-19 12:47:27 +0700

    database

--- database/project_akhir.sql
+++ database/project_akhir.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 19, 2022 at 04:51 AM
--- Server version: 10.4.22-MariaDB
--- PHP Version: 8.0.13
+-- Generation Time: Nov 19, 2022 at 06:47 AM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,99 +24,27 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `keranjang`
---
-
-CREATE TABLE `keranjang` (
-  `keranjangid` int(11) NOT NULL,
-  `orderlineid` int(11) NOT NULL,
-  `username` varchar(20) NOT NULL,
-  `total_harga` int(10) NOT NULL,
-  `orderstatusid` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `orderline`
---
-
-CREATE TABLE `orderline` (
-  `orderlineid` int(11) NOT NULL,
-  `productid` int(11) NOT NULL,
-  `quantity` int(3) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `orderstatus`
---
-
-CREATE TABLE `orderstatus` (
-  `orderstatusid` int(11) NOT NULL,
-  `status` varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `product`
 --
 
 CREATE TABLE `product` (
   `productid` int(11) NOT NULL,
   `name` varchar(200) NOT NULL,
-  `price` int(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user`
---
-
-CREATE TABLE `user` (
-  `username` varchar(20) NOT NULL,
-  `password` varchar(20) NOT NULL,
-  `telp` int(20) NOT NULL,
-  `alamat` text NOT NULL,
-  `level` varchar(20) NOT NULL
+  `price` int(10) NOT NULL,
+  `foto` text NOT NULL,
+  `penjelasan` varchar(105) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `user`
+-- Dumping data for table `product`
 --
 
-INSERT INTO `user` (`username`, `password`, `telp`, `alamat`, `level`) VALUES
-('admin', 'admin', 812345678, 'wates, kulon progo', 'admin'),
-('user', 'user', 82131414, 'jepara, jawa tengah', '');
+INSERT INTO `product` (`productid`, `name`, `price`, `foto`, `penjelasan`) VALUES
+(1, 'Auto Clave', 4000000, 'autoclave.jpg', 'Autoclave adalah salah satu alat laboratorium yang digunakan untuk mensterilkan alat-alat laboratorium se');
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `keranjang`
---
-ALTER TABLE `keranjang`
-  ADD PRIMARY KEY (`keranjangid`),
-  ADD KEY `orderlineid` (`orderlineid`),
-  ADD KEY `username` (`username`),
-  ADD KEY `orderstatusid` (`orderstatusid`);
-
---
--- Indexes for table `orderline`
---
-ALTER TABLE `orderline`
-  ADD PRIMARY KEY (`orderlineid`),
-  ADD KEY `orderline_ibfk_1` (`productid`);
-
---
--- Indexes for table `orderstatus`
---
-ALTER TABLE `orderstatus`
-  ADD PRIMARY KEY (`orderstatusid`);
 
 --
 -- Indexes for table `product`
@@ -125,56 +53,14 @@ ALTER TABLE `product`
   ADD PRIMARY KEY (`productid`);
 
 --
--- Indexes for table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`username`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
-
---
--- AUTO_INCREMENT for table `keranjang`
---
-ALTER TABLE `keranjang`
-  MODIFY `keranjangid` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `orderline`
---
-ALTER TABLE `orderline`
-  MODIFY `orderlineid` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `orderstatus`
---
-ALTER TABLE `orderstatus`
-  MODIFY `orderstatusid` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `productid` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `keranjang`
---
-ALTER TABLE `keranjang`
-  ADD CONSTRAINT `keranjang_ibfk_1` FOREIGN KEY (`orderlineid`) REFERENCES `orderline` (`orderlineid`),
-  ADD CONSTRAINT `keranjang_ibfk_2` FOREIGN KEY (`username`) REFERENCES `user` (`username`),
-  ADD CONSTRAINT `keranjang_ibfk_3` FOREIGN KEY (`orderstatusid`) REFERENCES `orderstatus` (`orderstatusid`);
-
---
--- Constraints for table `orderline`
---
-ALTER TABLE `orderline`
-  ADD CONSTRAINT `orderline_ibfk_1` FOREIGN KEY (`productid`) REFERENCES `product` (`productid`);
+  MODIFY `productid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

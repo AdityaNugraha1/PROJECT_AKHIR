5c2e479e1f3130999acfbbfd092dec908410eca7
AdityaNugraha1 <adit.toyan27@gmail.com>
2022-11-24 19:55:04 +0700

    index mlaku dewe

--- index.php
+++ index.php
@@ -37,19 +37,29 @@
       </div>
     </div>
   </nav>
-
- <div>
-   <img src="asset/logo-panjang.png">
- </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
-    </button>
+  <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active" data-bs-interval="10000">
+      <img src="asset/slide1.jpg" class="d-block w-100 h-100" alt="...">
+    </div>
+    <div class="carousel-item" data-bs-interval="2000">
+      <img src="asset/slide2.jpg" class="d-block w-100 h-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="asset/logo-panjang.png" class="d-block w-100 h-100" alt="...">
+    </div>
   </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+
 <div id="produk">
   <div class="p-3 m-0 border-0">
     <div class="container text-center">
